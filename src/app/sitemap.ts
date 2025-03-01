84563e60320c26f5e07e1dd39e58cafab52b5878
Muhammed Ali <70213359+mamiiblt@users.noreply.github.com>
2025-03-01 21:30:01 +0000

    Fix issues on sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,29 +1,23 @@
-import { MetadataRoute } from "next";
-import { getAllPosts } from "@/lib/blog";
+import { MetadataRoute } from 'next'
+import { getAllPosts } from '@/lib/blog'
 
-const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "";
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || ''
 if (!baseUrl) {
-  throw new Error("NEXT_PUBLIC_SITE_URL is not defined");
+  throw new Error('NEXT_PUBLIC_SITE_URL is not defined')
 }
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const posts = await getAllPosts();
+  const posts = await getAllPosts()
 
   // Base routes
-  const routes = ["", "/about", "/projects", "/blogs"].map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date(),
-    changeFrequency: "monthly" as const,
-    priority: route === "" ? 1 : 0.8,
-  }));
+  const routes = ['', '/library_backup', '/about_updater', '/download'].map(
+    (route) => ({
+      url: `${baseUrl}${route}`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as const,
+      priority: route === '' ? 1 : 0.8,
+    })
+  )
 
-  // Blog posts
-  const postRoutes = posts.map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: new Date(post.date),
-    changeFrequency: "daily" as const,
-    priority: 0.6,
-  }));
-
-  return [...routes, ...postRoutes];
+  return [...routes]
 }
